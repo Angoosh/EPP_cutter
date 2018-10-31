bc808ed77f5de3bd40c22f61a967edb0694be141
Angoosh <kaplanantonin@gmail.com>
2018-10-31 21:15:48 +0100

    freezing issue fix
    moving of motors without freezing GUI works now

--- src/GUI.py
+++ src/GUI.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env p#ython3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 23 13:51:44 2018
 
 @author: angoosh
+
+Must run on python 3 
 """
 
+from threading import Thread
 from kivy.app import App
 from kivy.graphics import Rectangle
 from kivy.uix.floatlayout import FloatLayout
@@ -15,83 +18,163 @@ from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 import Gcode
+import config
 from Gcode import connection
 import instructions as i
 from byteparams import parameter as par
-from time import sleep
+import os
 
 
 
-def end():
-    Gui().stop()
+#nacitani konfiguracnich promennych do globalnich promennych
+mode = config.glob()[0]
+travel = config.glob()[1]
+negtravel = config.glob()[2]
+    #promenne pro kontrolu, zda prislusna osa jede
+x1 = 0
+y1 = 0
+z1 = 0
+a1 = 0
 
 
-#ser = "/dev/ttyACM0"
-  
+#funkce cekani na dojeti motoru na pozici
+def wait_until_reached():
+    while True:
+        global x1, y1, z1, a1
+        r = ser.read(9)
+        print(r)
+        if r == b'\x02\x01\x80\x8a\x00\x00\x00\x01\x0e':
+            print("travelled")
+            x1 = y1 = z1 = a1 = 0
+            break
+        if r == b'\x02\x01\x80\x8a\x00\x00\x00\x02\x0f':
+            x1 = y1 = z1 = a1 = 0
+            break
+        if r == b'\x02\x01\x80\x8a\x00\x00\x00\x04\x11':
+            x1 = y1 = z1 = a1 = 0
+            break
+        if r == b'\x02\x01\x80\x8a\x00\x00\x00\x08\x15':
+            x1 = y1 = z1 = a1 = 0
+            break        
+
+#trida pro nahrazeni ser.write(), kdyz bude port nedostupny
 class Exceptions:
     def write(self,byte):
         print("No Connection")
-        
-#Globalni promenne
+    def read(dd, aa):
+        print("No connection")
 ser = Exceptions()
-travel = 10
-negtravel = -10
-mode = "ABS"
 
+#trida vsech objektu GUI
 class Widgets(FloatLayout):
     def __init__(self, **kwargs):
         super(Widgets, self).__init__(**kwargs)
-        
+            
 #funkce pro pohybova tlacitka
         def Xa(instance):
+            global x1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 0, travel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
-            print(b1,b2,b3,b4,b5,b6,b7,b8,b9)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis X ascend: " + str(travel) +"mm\n"
+            if x1 == 0:
+                x1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,1,140]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis X ascend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis X already moving\n"
         def Xd(instance):
+            global x1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 0, negtravel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis X descend: " + str(travel) +"mm\n"
+            if x1 == 0:
+                x1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,1,140]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis X descend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis X already moving\n"
         def Ya(instance):
+            global y1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 1, travel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis Y ascend: " + str(travel) +"mm\n"
+            if y1 == 0:
+                y1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,2,141]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis Y ascend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis Y already moving\n"
         def Yd(instance):
+            global y1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 1, negtravel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis Y descend: " + str(travel) +"mm\n"
+            if y1 == 0:
+                y1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,2,141]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis Y descend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis Y already moving\n"
         def Za(instance):
+            global z1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 2, travel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis Z ascend: " + str(travel) +"mm\n"
+            if z1 == 0:
+                z1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,4,143]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis Z ascend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis Z already moving\n"
         def Zd(instance):
+            global z1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 2, negtravel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis Z descend: " + str(travel) +"mm\n"
+            if z1 == 0:
+                z1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,4,143]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis Z descend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis Z already moving\n"
         def Aa(instance):
+            global a1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 3, travel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis A ascend: " + str(travel) +"mm\n"
+            if a1 == 0:
+                a1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,8,147]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis A ascend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis A already moving"
         def Ad(instance):
+            global a1
             b2, b3, b4, b5, b6, b7, b8 = i.MVP("REL", 3, negtravel)
             b1,b2,b3,b4,b5,b6,b7,b8,b9 = par(1,b2,b3,b4,b5,b6,b7,b8)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
-            ser.write(x)
-            self.terminal.text += "axis A descend: " + str(travel) +"mm\n"
+            if a1 == 0:
+                a1 = 1
+                ser.write(bytearray([1,138,0,0,0,0,0,8,147]))
+                ser.write(x)
+                Thread(target = wait_until_reached).start()
+                self.terminal.text += "axis A descend: " + str(travel) +"mm\n"
+            else:
+                self.terminal.text += "axis A already moving\n"
         
 #funkce stop tlacitka    
         def STOP(instance):
@@ -121,10 +204,11 @@ class Widgets(FloatLayout):
                 self.terminal.text += "Feedrate set to: " + self.feed.text + "\n"
 #funkce pro travel os
         def trav(btn):
+            global travel
             if self.travel.text == "":
                 travel = 0
             else:
-                global travel
+                
                 travel = float(self.travel.text)
                 global negtravel
                 negtravel = travel * -1
@@ -133,13 +217,31 @@ class Widgets(FloatLayout):
             return travel
 #funkce pro nacteni gcodu
         def gcoload(btn):
+            global file
             try:
-                with open(self.gco1.text) as f:
-                    content1 = f.read()
+                with open(self.gco.text) as f:
+                    content = f.read()
                     self.terminal.text += "Gcode loaded\n"
-                    print(content1)
+                    print(content)
             except:
-                self.terminal.text += "Path: "+self.gco1.text+" does not exist\n"
+                self.terminal.text += "Path: "+self.gco.text+" does not exist\n"
+                
+#funkce spusteni senderu
+        def cut(instance):
+            try:
+                if len(file) > 2:
+                    pass
+            except:
+                self.terminal.text += "No file loaded\n"
+            try:
+                if len(ser) > 2:
+                    pass
+            except:
+                self.terminal.text += "No connection to serial\n"
+            try:
+                os.system("python sender.py")
+            except:
+                self.terminal.text += "No sender program\n"
 #funkce vycisteni terminalu                
         def clr(instance):
             self.terminal.text = ""
@@ -156,13 +258,13 @@ class Widgets(FloatLayout):
 #funkce primeho uzivatelskeho vstupu
         def command(instance):
             i = self.command.text
+            global mode
             if i.find("G0") != -1:
                 print("G0")
                 x = i.find("X")
                 y = i.find("Y")
                 z = i.find("Z")
                 a = i.find("A")
-                print(x, y, z, a)
                 if (x == -1) or (y == -1) or (z == -1) or (a == -1):
                     self.terminal.text += "Command must be in form: G0 X Y Z A\n"
                     return
@@ -187,7 +289,6 @@ class Widgets(FloatLayout):
                 y = i.find("Y")
                 z = i.find("Z")
                 a = i.find("A")
-                print(x, y, z, a)
                 if (x == -1) or (y == -1) or (z == -1) or (a == -1):
                     self.terminal.text += "Command must be in form: G1 X Y Z A\n"
                     return
@@ -222,7 +323,7 @@ class Widgets(FloatLayout):
                 self.terminal.text += i + "\n"
                 self.command.text = ""
             elif i.find("G91") != -1:
-                global mode
+                
                 mode = Gcode.G91()
                 print("G91")
                 self.terminal.text += i + "\n"
@@ -246,12 +347,15 @@ class Widgets(FloatLayout):
                 print("MST")
                 self.terminal.text += i + "\n"
                 self.command.text = ""
+            elif i == "":
+                self.terminal.text += "No command specified\n"
+                
             else:
                 self.terminal.text += "Unknown command: " + self.command.text + "\n"
                 self.command.text = ""
-            
 #funkce nastaveni portu        
         def port(instance):
+            global ser
             if self.portButt.state == "down":
                 self.portButt.text = "Disconnect"
                 self.portButt.size_hint = (.1, .05)
@@ -261,7 +365,7 @@ class Widgets(FloatLayout):
                     if ser != "":
                         print(ser.name)
                         self.terminal.text += "Connected to: "+ser.name+"\n"
-                        global ser
+                        #global ser
                         ser = ser
                         return ser
                         #ser = connection(self.port.text)
@@ -285,25 +389,21 @@ class Widgets(FloatLayout):
             b1,b2,b3,b4,b5,b6,b7,b8,b9=par(1,13,0,0,0,0,0,0)
             x = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
             ser.write(x)
-            print(x)
             self.terminal.text += "Home X\n"
         def homeY(instance):
             b1,b2,b3,b4,b5,b6,b7,b8,b9=par(1,13,0,1,0,0,0,0)
             y = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
             ser.write(y)
-            print(y)
             self.terminal.text += "Home Y\n"
         def homeZ(instance):
             b1,b2,b3,b4,b5,b6,b7,b8,b9=par(1,13,0,2,0,0,0,0)
             z = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
             ser.write(z)
-            print(z)
             self.terminal.text += "Home Z\n"
         def homeA(instance):
             b1,b2,b3,b4,b5,b6,b7,b8,b9=par(1,13,0,3,0,0,0,0)
             a = bytearray([b1,b2,b3,b4,b5,b6,b7,b8,b9])
             ser.write(a)
-            print(a)
             self.terminal.text += "Home A\n"
         
 #tlacitka pohybu
@@ -349,13 +449,17 @@ class Widgets(FloatLayout):
                 Button(text = "set", on_press = trav, font_size = 25, size_hint=(.05, .05), pos = (650, 400)))
        
 #file chooser
-        self.gco1 = TextInput(size_hint=(.2, .05), pos=(100, 480), font_size=12, multiline=False)
-        self.add_widget(self.gco1)
+        self.gco = TextInput(size_hint=(.2, .05), pos=(100, 480), font_size=12, multiline=False)
+        self.add_widget(self.gco)
         self.add_widget(
                 Label(text = "Gcode", pos = (100, 510), size_hint=(.1, .05), color=(0,0,0,255)))
         self.add_widget(
                 Button(text = "Load", on_press = gcoload, font_size = 25, size_hint=(.08, .05), pos = (370, 480)))
        
+#tlacitko zacnuti vyrezavani
+        self.add_widget(
+                Button(text = "Cut", on_press = cut, font_size = 25, size_hint=(.05, .05), pos = (100, 400)))
+        
 #tlacitko nahrati odporoveho dratu
         self.heating = ToggleButton(text = "Heating", on_press = heating, font_size = 25, size_hint=(.08, .05), pos = (500, 480), background_down = "./images/heatingPressed.png")
         self.add_widget(self.heating)
@@ -392,7 +496,7 @@ class Widgets(FloatLayout):
         self.portButt = ToggleButton(text = "Connect", on_press = port, font_size = 25, size_hint=(.08, .05), pos = (240, 560))
         self.add_widget(self.portButt)
         
-    
+#trida samotne aplikace   
 class Gui(App):
      def build(self):
         self.root = root = Widgets()
@@ -401,6 +505,7 @@ class Gui(App):
         Window.size = (1280, 720)
         self.icon = "./images/appIcon1.png"
         self.title = "Control Panel"
+        self.window_icon = "./images/appIcon1.png"
 
         with root.canvas.before:
             self.rect = Rectangle(size=root.size, pos=root.pos)
@@ -409,7 +514,8 @@ class Gui(App):
      def _update_rect(self, instance, value):
         self.rect.pos = instance.pos
         self.rect.size = instance.size
-        
+
+#spusteni aplikace        
 if __name__ == "__main__":
     Gui().run()
     

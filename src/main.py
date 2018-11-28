e7562c5801d85a343a4e59946374d90d4d401598
Angoosh <kaplanantonin@gmail.com>
2018-11-28 17:38:17 +0100

    added automation script

---
+++ src/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#skript automaticke kontroly a spusteni aplikace
+import os
+import subprocess
+
+for i in range (0,3):
+    
+    #kontrola modulů
+    try:
+        import serial
+    except:
+        print("Module pyserial is not installed. Try: pip3 install pyserial\nand pip install pyserial")
+        break
+    
+    #kontrola, jestli bezi na RPi
+    if os.path.exists("/home/pi"):
+        subprocess.call(["clear"], shell = True)
+        subprocess.call(["python3 CLI.py"], shell = True)
+        break
+    
+    #kontrola, zda je zaple GUI os / ssh s parametrem -X a zda je nainstalovane kivy jinak se spusti CLI
+    try:
+        os.environ['DISPLAY']
+        import kivy
+        subprocess.call(["python3 GUI.py"], shell = True)
+    except:
+        subprocess.call(["clear"], shell = True)
+        subprocess.call(["python3 CLI.py"], shell = True)
+    break

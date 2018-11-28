bbeea88321ce9cf91ccea4976870c322a5d41879
Angoosh <kaplanantonin@gmail.com>
2018-11-28 17:23:34 +0100

    added automated startup

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -4,3 +4,8 @@ sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt update
 sudo apt-get install python-kivy python3-kivy python3-pip
 pip3 install pyserial
+cd ~/cutter/src
+chmod +x main.py
+cd ~/
+echo "cd ~/cutter/src" >> .bashrc
+echo "./main.py" >> .bashrc

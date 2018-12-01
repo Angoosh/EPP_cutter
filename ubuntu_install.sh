c57ae817f896ab6b942c9d3f06d32d91c528cf99
Angoosh <kaplanantonin@gmail.com>
2018-12-01 09:32:04 +0100

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository ppa:kivy-team/kivy -y
 sudo apt update
-sudo apt-get install python-kivy python3-kivy python3-pip
+sudo apt-get install python-kivy python3-kivy python3-pip -y
 pip3 install pyserial
 cd ~/cutter/src
 chmod +x main.py

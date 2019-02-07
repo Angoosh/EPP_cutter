5fe513fd413f471ba0828372c38655c8a4ec290c
Angoosh <kaplanantonin@gmail.com>
2019-02-07 12:53:45 +0100

    Update EMERGENCY.py

--- src/EMERGENCY.py
+++ src/EMERGENCY.py
@@ -18,7 +18,7 @@ def EMERGENCY_STOP():
     os.system("rm comm.pickle")
     os.system("kill "+str(pid))
     GPIO.output(4, GPIO.LOW)
-    os.system("kill +"str(self_pid))
+    os.system("kill "+str(self_pid))
 
 if __name__ == "__main__":
     try:

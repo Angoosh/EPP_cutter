baab3ff6e29348537d9a436f06185bae78b7d357
Angoosh <kaplanantonin@gmail.com>
2019-03-05 10:31:01 +0100

    Update EMERGENCY.py

--- src/EMERGENCY.py
+++ src/EMERGENCY.py
@@ -5,20 +5,24 @@ import pickle
 import serial
 import Gcode
 
+#otevreni souboru s informaci o portu a pid senderu
 f = open("comm.pickle", "rb")
 pid = pickle.load(f)
 serPort = pickle.load(f)
 print("EMERGENCY PID CALLBACK:  "+str(pid))
 f.close()
 
+#ziskani pid vlastniho procesu
 self_pid = os.getpid()
 print("EM: Got PID")
 
+#ulozeni pid vlastniho procesu do souboru
 f = open("emergency.pickle", "wb")
 pickle.dump(self_pid, f)
 f.close()
 print("EM: Dumped PID")
 
+#funkce nouzoveho tlacitka
 def EMERGENCY_STOP(x):
     print("EM: Stopping sender")
     os.system("rm comm.pickle")
@@ -33,6 +37,7 @@ def EMERGENCY_STOP(x):
 
 if __name__ == "__main__":
     try:
+        #nastaveni cteni tlacitka a zapnuti indikace
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)

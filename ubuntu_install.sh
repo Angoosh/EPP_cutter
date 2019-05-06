7724b4788c7f45196c8c932fc879c48356fc6c1e
Angoosh <kaplanantonin@gmail.com>
2019-05-06 16:32:52 +0200

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:kivy-team/kivy -y
-sudo apt update
-sudo apt-get install python-kivy python3-kivy python3-pip -y
+read -p "Do you want GUI? Y/n" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   sudo add-apt-repository ppa:kivy-team/kivy -y
+   sudo apt update
+   sudo apt install python-kivy python3-kivy
+else
+   sudo apt update
+fi
+
+sudo apt upgrade
+sudo apt-get install python3-pip -y
 pip3 install pyserial
 cd ~/cutter/src
 chmod +x main.py
